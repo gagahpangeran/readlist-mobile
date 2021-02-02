d732d1506c720f7d99e16645e247ee85bd1484c3
Gagah Pangeran <gagahpangeran@gmail.com>
2021-02-02 13:33:11 +0700

    add setting model

---
+++ lib/models/setting.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Setting {
+  String _apiKey;
+  String _gistId;
+  String _fileName;
+
+  Setting._(this._apiKey, this._gistId, this._fileName);
+
+  String get apiKey => _apiKey;
+  String get gistId => _gistId;
+  String get fileName => _fileName;
+
+  static Future<Setting> create({
+    @required String apiKey,
+    @required String gistId,
+    @required String fileName,
+  }) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('apiKey', apiKey);
+    prefs.setString('gistId', gistId);
+    prefs.setString('fileName', fileName);
+
+    return Setting._(apiKey, gistId, fileName);
+  }
+
+  static Future<Setting> load() async {
+    final prefs = await SharedPreferences.getInstance();
+    String apiKey = prefs.getString('apiKey');
+    String gistId = prefs.getString('gistId');
+    String fileName = prefs.getString('fileName');
+
+    return Setting._(apiKey, gistId, fileName);
+  }
+}
