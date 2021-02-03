a712aaa40a51d2eb9ccaee685b27f5ed9d88682d
Gagah Pangeran <gagahpangeran@gmail.com>
2021-02-03 10:41:58 +0700

    add snackbar in form page

--- lib/pages/form.dart
+++ lib/pages/form.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:readlist/api/gist.dart';
-import 'package:readlist/components/loading_screen.dart';
 import 'package:readlist/models/read_list_item.dart';
 
 class FormPage extends StatefulWidget {
@@ -12,24 +11,60 @@ class _FormPageState extends State<FormPage> {
   String _link;
   String _title;
   bool _isRead = true;
-  bool _isLoading = false;
 
   GistAPI api = GistAPI();
 
-  _submitForm() async {
-    setState(() => _isLoading = true);
+  Future<bool> _submitForm() async {
     final readListItem = ReadListItem(
       link: _link,
       title: _title,
       isRead: _isRead,
     );
-    bool success = await api.submitData(readListItem);
 
-    if (success) {
-      Navigator.pop(context);
+    try {
+      await api.submitData(readListItem);
+      return true;
+    } catch (e) {
+      return false;
     }
+  }
 
-    setState(() => _isLoading = false);
+  Widget _buildSnackbar({
+    @required String text,
+    void Function() action,
+    String actionLabel,
+  }) {
+    return SnackBar(
+      content: Text(text),
+      behavior: SnackBarBehavior.floating,
+      action: action == null
+          ? null
+          : SnackBarAction(label: actionLabel, onPressed: action),
+    );
+  }
+
+  _onSubmit() async {
+    final scaffold = ScaffoldMessenger.of(context);
+
+    final submitSnackbar = _buildSnackbar(text: 'Submitting data...');
+
+    final errorSnackbar = _buildSnackbar(
+      text: 'Error! Can\'t submit data',
+      action: () => scaffold.hideCurrentSnackBar(),
+      actionLabel: 'Close',
+    );
+
+    final successSnackbar = _buildSnackbar(text: 'Success submit data!');
+
+    scaffold.showSnackBar(submitSnackbar);
+    bool success = await _submitForm();
+    scaffold.hideCurrentSnackBar();
+    if (success) {
+      scaffold.showSnackBar(successSnackbar);
+      Navigator.pop(context);
+    } else {
+      scaffold.showSnackBar(errorSnackbar);
+    }
   }
 
   @override
@@ -39,44 +74,42 @@ class _FormPageState extends State<FormPage> {
         title: Text('Add New List'),
       ),
       body: Form(
-        child: _isLoading
-            ? LoadingScreen()
-            : Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.link),
-                      labelText: 'Link',
-                    ),
-                    onChanged: (value) => setState(() => _link = value),
-                    validator: (value) =>
-                        value.isEmpty ? 'Please enter the link' : null,
-                  ),
-                  TextFormField(
-                    onChanged: (value) => setState(() => _title = value),
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.title),
-                      labelText: 'Title',
-                    ),
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Checkbox(
-                        value: _isRead,
-                        onChanged: (value) => setState(() => _isRead = value),
-                      ),
-                      Text('Already Read'),
-                    ],
-                  ),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () => _submitForm(),
-                      child: Text('Submit'),
-                    ),
-                  ),
-                ],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: Icon(Icons.link),
+                labelText: 'Link',
               ),
+              onChanged: (value) => setState(() => _link = value),
+              validator: (value) =>
+                  value.isEmpty ? 'Please enter the link' : null,
+            ),
+            TextFormField(
+              onChanged: (value) => setState(() => _title = value),
+              decoration: const InputDecoration(
+                icon: Icon(Icons.title),
+                labelText: 'Title',
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Checkbox(
+                  value: _isRead,
+                  onChanged: (value) => setState(() => _isRead = value),
+                ),
+                Text('Already Read'),
+              ],
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: _onSubmit,
+                child: Text('Submit'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
