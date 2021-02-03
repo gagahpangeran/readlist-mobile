ef270adc23c008d2fcba5d37c82dbbcaa44f3584
Gagah Pangeran <gagahpangeran@gmail.com>
2021-02-03 11:52:15 +0700

    create helper class

---
+++ lib/utils/helper.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class Helper {
+  Helper._();
+
+  static SnackBar _buildSnackbar({
+    @required String text,
+    void Function() action,
+    String actionLabel,
+  }) {
+    return SnackBar(
+      content: Text(text),
+      behavior: SnackBarBehavior.floating,
+      action: action == null
+          ? null
+          : SnackBarAction(label: actionLabel, onPressed: action),
+    );
+  }
+
+  static void submitForm({
+    @required BuildContext context,
+    @required Future<bool> submitFunction(),
+    @required String onSubmitText,
+    @required String onSuccessText,
+    @required String onErrorText,
+  }) async {
+    final scaffold = ScaffoldMessenger.of(context);
+    final onSubmitSnackbar = _buildSnackbar(text: onSubmitText);
+    final onSuccessSnackbar = _buildSnackbar(text: onSuccessText);
+    final onErrorSnackbar = _buildSnackbar(
+      text: onErrorText,
+      action: () => scaffold.hideCurrentSnackBar(),
+      actionLabel: 'Close',
+    );
+
+    scaffold.showSnackBar(onSubmitSnackbar);
+    bool success = await submitFunction();
+    scaffold.hideCurrentSnackBar();
+    if (success) {
+      scaffold.showSnackBar(onSuccessSnackbar);
+      Navigator.pop(context);
+    } else {
+      scaffold.showSnackBar(onErrorSnackbar);
+    }
+  }
+}
