1e1d74304e01cf5447d3a3c9cd84275eddb2ed1b
Gagah Pangeran <gagahpangeran@gmail.com>
2021-02-04 14:48:05 +0700

    add date and open link in read list tile

--- lib/components/read_list_tile.dart
+++ lib/components/read_list_tile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:readlist/models/read_list_item.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ReadListTile extends StatefulWidget {
   final ReadListItem item;
@@ -11,17 +13,29 @@ class ReadListTile extends StatefulWidget {
 }
 
 class _ReadListTileState extends State<ReadListTile> {
+  final format = DateFormat('yyyy-MM-dd, HH:mm:ss');
+
+  _launchLink() async {
+    final url = widget.item.link;
+    if (await canLaunch(url)) {
+      await launch(url);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       minVerticalPadding: 16.0,
       title: Text(widget.item.title),
+      subtitle: Text("""Updated At : ${format.format(widget.item.updatedAt)}
+Created At : ${format.format(widget.item.createdAt)}"""),
       trailing: widget.item.isRead
           ? Icon(
               Icons.check,
               color: Colors.blue,
             )
           : null,
+      onTap: _launchLink,
     );
   }
 }
