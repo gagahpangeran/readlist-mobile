6d55af57f7181c22ffd5533a7c541d13d0ed18fd
Gagah Pangeran <git@gagahpangeran.com>
2021-04-01 09:12:45 +0700

    add graphql client in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:readlist/pages/form.dart';
 import 'package:readlist/pages/list.dart';
 import 'package:readlist/pages/setting.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  final HttpLink link = HttpLink('http://localhost:9000/graphql');
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: link,
+      cache: GraphQLCache(store: InMemoryStore()),
+    ),
+  );
+
+  runApp(MyApp(client: client));
+}
 
 class MyApp extends StatelessWidget {
+  final ValueNotifier<GraphQLClient> client;
+
+  MyApp({required this.client});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Read List',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return GraphQLProvider(
+      client: client,
+      child: CacheProvider(
+        child: MaterialApp(
+          title: 'Read List',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          initialRoute: '/list',
+          routes: {
+            '/list': (context) => ListPage(),
+            '/form': (context) => FormPage(),
+            '/setting': (content) => SettingPage(),
+          },
+        ),
       ),
-      initialRoute: '/list',
-      routes: {
-        '/list': (context) => ListPage(),
-        '/form': (context) => FormPage(),
-        '/setting': (content) => SettingPage(),
-      },
     );
   }
 }
