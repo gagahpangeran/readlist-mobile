4918e4a60fe600e0b5b410ce81d0fb3f3b06a394
Gagah Pangeran <git@gagahpangeran.com>
2021-04-05 11:56:59 +0700

    use graphql wrapper in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:readlist/components/gql_wrapper.dart';
 import 'package:readlist/pages/form.dart';
 import 'package:readlist/pages/list.dart';
 import 'package:readlist/pages/setting.dart';
 
-void main() async {
-  final HttpLink link = HttpLink('http://localhost:9000/graphql');
-
-  ValueNotifier<GraphQLClient> client = ValueNotifier(
-    GraphQLClient(
-      link: link,
-      cache: GraphQLCache(store: InMemoryStore()),
-    ),
-  );
-
-  runApp(MyApp(client: client));
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final ValueNotifier<GraphQLClient> client;
-
-  MyApp({required this.client});
-
   @override
   Widget build(BuildContext context) {
-    return GraphQLProvider(
-      client: client,
-      child: CacheProvider(
-        child: MaterialApp(
-          title: 'Read List',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-          ),
-          initialRoute: '/list',
-          routes: {
-            '/list': (context) => ListPage(),
-            '/form': (context) => FormPage(),
-            '/setting': (content) => SettingPage(),
-          },
+    return GQLWrapper(
+      app: MaterialApp(
+        title: 'Read List',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
+        initialRoute: '/list',
+        routes: {
+          '/list': (context) => ListPage(),
+          '/form': (context) => FormPage(),
+          '/setting': (content) => SettingPage(),
+        },
       ),
     );
   }
